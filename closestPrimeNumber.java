cb64951bee68f8d832b950d78c282530bec45a45
Sahib Singh <51374188+SahibSingh1311@users.noreply.github.com>
2022-08-03 21:03:52 +0530

    Add files via upload

---
+++ closestPrimeNumber.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class closestPrimeNumber{
+	public static void main(String[] args){
+	int num = 10;
+	System.out.println(closestPrime(num));
+}
+	public static boolean isPrime(int n){
+	if(n==0)
+		return false;
+	if(n==1)
+		return false;
+	if(n==2)
+		return true;
+	if(n%2==0)
+		return false;
+	for(int i=3;i<n/2;i+=2){
+		if(n%i==0)
+			return false;
+	}
+	return true;
+}
+	public static ArrayList<Integer> closestPrime(int num){
+	
+	boolean small = true;
+	boolean big = true;
+	int smalln=num;
+	int bign=num;
+	ArrayList<Integer> ans=new ArrayList();
+	
+	if(isPrime(num))
+		ans.add(num);	
+
+	while(big || small){
+	if(big){
+		if(isPrime(bign)){
+			big = false;
+			ans.add(bign);
+		}
+	bign++;
+}
+	if(small){
+		if(isPrime(smalln)){
+			small = false;
+			ans.add(smalln);
+		}
+		smalln--;
+}
+}	
+	return ans;
+}
+}
